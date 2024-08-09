a289aa5ead7ac9e505ca5dac5cf3afb0fb8b62a5
maruki00 <aitbaha48@gmail.com>
2024-08-09 10:10:42 +0300

    --

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("hello world"))
+	})
+
+	http.ListenAndServe(":3000", nil)
 
 }
---
+++ internal/domain/ports/IpnutPort.go
@@ -0,0 +1,4 @@
+package ports
+
+type InputPort interface {
+}
